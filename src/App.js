c9007819ab333deee8082420ce91d710e26819de
samir <samiralurhp@gmail.com>
2020-06-05 16:51:15 +0530

    React Unit test sample

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  decrement = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div className="App">
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment C</button>
+        <button className="decrement" onClick={this.increment}>Increment count</button>
+      </div>
+    );
+  }
+}
+
+export default App;
